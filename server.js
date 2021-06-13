c23b4f76775b680923a67aa645e1c729d9ee42da
M Natoli <natoli.missy@gmail.com>
2021-06-13 13:40:11 -0500

    created models and associations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+//D import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+//D sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
+  sequelize.sync(() => {
+    console.log('Connected to database');
+  })
   console.log(`App listening on port ${PORT}!`);
 });
